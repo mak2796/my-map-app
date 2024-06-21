2d2ba8bfcf4981e0a37924820fbe06e1f972295d
Mohd Aman Qazi <qaziaman095@gmail.com>
2024-06-21 21:42:02 +0530

    Legend fix

--- src/feature/MapView.js
+++ src/feature/MapView.js
@@ -105,6 +105,7 @@ const MapView = () => {
         <SearchControl />
         <HomeButton />
         <ZoomControl position="topright" />
+        <Legend/>
         {renderCircles()}
       </MapContainer>
     </div>
